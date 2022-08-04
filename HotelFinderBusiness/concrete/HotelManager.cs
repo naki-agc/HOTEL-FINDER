eb52030f0d20b08b1b17c3ca23743dcbc026235d
Ali Naki AĞCA <56757817+naki-agc@users.noreply.github.com>
2022-08-04 21:36:46 +0300

    Add files via upload

---
+++ HotelFinderBusiness/concrete/HotelManager.cs
@@ -0,0 +1,50 @@
+﻿using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.concrete;
+using HotelFinder.Entities;
+using HotelFinderBusiness.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinderBusiness.concrete
+{
+    public class HotelManager : IHotelService
+    {
+        private IHotelRepository _hotelRepository;
+
+        public HotelManager()
+        {
+            _hotelRepository = new HotelRepository();
+        }
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            return _hotelRepository.CreateHotel(hotel);
+        }
+
+        public void DeleteHotel(int id)
+        {
+            _hotelRepository.DeleteHotel(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            if (id>0)
+            {
+                return _hotelRepository.GetHotelsById(id);
+            }
+            throw new Exception("id can not be less than 1");
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
