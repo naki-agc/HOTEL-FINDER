d41affaafb2c72a939eb5620b227d2e74fb9580b
Ali Naki AĞCA <56757817+naki-agc@users.noreply.github.com>
2022-08-04 21:32:35 +0300

    Add files via upload

---
+++ HotelFinder.Entities/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelFinder.Entities
+{
+    public class Hotel
+    {
+
+        //Annotations
+        //database ve id alanımız primarykey ide ve birden başlayıp artacak demekidentity 
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string City{ get; set; }
+    }
+}
